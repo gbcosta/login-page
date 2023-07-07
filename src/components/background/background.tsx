218f2f8c204af19d5973d811e536401c8877e7ec
gbcostasant@gmail.com <gbcostasant@gmail.com>
2023-07-07 20:01:29 -0300

    update

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -1,8 +1,7 @@
 const Background = () => {
   return (
-    <div className="h-screen">
-      <div className="bg-cover h-full w-full bg-no-repeat bg-center bg-[url('./assets/bg.jpg')]"></div>
-      ;
+    <div className="h-screen w-screen">
+      <div className="bg-cover h-full  bg-no-repeat bg-center bg-[url('./assets/bg.jpg')]"></div>
     </div>
   );
 };
--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,8 +1,12 @@
 import logo from "../../assets/logo.png";
+import { FcGoogle } from "react-icons/fc";
+import { FaApple } from "react-icons/fa";
+import { IconType } from "react-icons";
 
-const Button = (props: { children: string; img?: string }): JSX.Element => {
+const Button = (props: { children: string; icon: IconType }): JSX.Element => {
   return (
-    <button className="border-black border-solid border-[3px] rounded-md p-2 font-bold">
+    <button className="border-black border-solid border-[3px] rounded-md p-2 font-bold flex justify-center items-center gap-4">
+      <props.icon style={{ textAlign: "center", fontSize: "2rem" }} />
       {props.children}
     </button>
   );
@@ -14,18 +18,18 @@ const Form = () => {
       <div className="mb-8">
         <img src={logo} className="w-16 h-16" />
         <h1 className="text-black font-bold text-4xl">Log In</h1>
-        <p className="text-zinc-400 ">
+        <p className="text-zinc-400 mt-2    ">
           Don't have an account?&nbsp;
           <span className="text-black font-bold ">Create an account</span>
         </p>
       </div>
       <div className="flex flex-col gap-2">
-        <Button>Continue with Google</Button>
-        <Button>Continue with Apple</Button>
+        <Button icon={FcGoogle}>Continue with Google</Button>
+        <Button icon={FaApple}>Continue with Apple</Button>
       </div>
       <div className="flex justify-center">
         <p className="text-center text-zinc-400 py-4 w-full flex justify-between items-center before:box-content before:w-[30%] before:h-[0.05rem] before:bg-zinc-400 before:block after:box-content after:w-[30%] after:h-[0.05rem] after:bg-zinc-400 after:block">
-          or
+          OR
         </p>
       </div>
       <form className="flex gap-4 flex-col">
@@ -43,7 +47,7 @@ const Form = () => {
         ></input>
         <button className="bg-black text-white p-2 rounded-md">Log In</button>
       </form>
-      <div className="flex flex-col my-8 gap-2">
+      <div className="flex flex-col my-8 gap-4">
         <p className="underline hover:no-underline cursor-pointer">
           Use single sign-on
         </p>
@@ -53,15 +57,26 @@ const Form = () => {
       </div>
 
       <p className="text-zinc-400 text-[0.9rem] pr-12">
-        By Clicking "Continue Apple/Goole", above you acknowledge that you have
+        By Clicking "Continue Apple/Goole", above you acknowledge thaat you have
         agree to our&nbsp;
         <span className="underline hover:no-underline cursor-pointer">
           General Term and Conditions
         </span>
-        and have read and acknowledge the&nbsp;
+        &nbsp; and have read and acknowledge the&nbsp;
         <span className="underline cursor-pointer hover:no-underline ">
           Privacy Policy
         </span>
+      </p>
+      <p className="text-zinc-400 text-[0.9rem] pr-12 mt-4">
+        This site is protected by reCAPTCHA and the&nbsp;
+        <span className="underline hover:no-underline cursor-pointer">
+          privacy policy
+        </span>
+        &nbsp;and&nbsp;
+        <span className="underline cursor-pointer hover:no-underline ">
+          Term of Service
+        </span>
+        apply
       </p>
       <div className="mt-auto gap-4 flex text-zinc-400">
         <a>Terms</a>
