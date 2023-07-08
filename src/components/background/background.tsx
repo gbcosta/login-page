fd3b8d6ed75cbb80fe7df6ffacd379953ca22dff
gbcostasant@gmail.com <gbcostasant@gmail.com>
2023-07-07 22:23:35 -0300

    add responsiveness

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -1,6 +1,6 @@
 const Background = () => {
   return (
-    <div className="h-screen w-screen">
+    <div className="sm:h-screen sm:w-screen h-0 w-0">
       <div className="bg-cover h-full  bg-no-repeat bg-center bg-[url('./assets/bg.jpg')]"></div>
     </div>
   );
--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -6,8 +6,8 @@ import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
 
 const Button = (props: { children: string; icon: IconType }): JSX.Element => {
   return (
-    <button className="border-black border-solid border-[3px] rounded-md p-2 font-bold flex justify-center items-center gap-4 duration-150 hover:bg-zinc-100">
-      <props.icon className="text-center text-3xl" />
+    <button className="sm:text-sm border-black border-solid border-[3px] rounded-md 2xl:p-2 p-1 font-bold flex justify-center items-center gap-4 duration-150 hover:bg-zinc-100">
+      <props.icon className="text-center 2xl:text-3xl text-2xl" />
       {props.children}
     </button>
   );
@@ -15,13 +15,15 @@ const Button = (props: { children: string; icon: IconType }): JSX.Element => {
 
 const Form = () => {
   return (
-    <div className="font-bold max-w-lg px-[4.2rem] py-12 h-screen flex flex-col">
-      <div className="mb-8">
+    <div className="font-bold 2xl:max-w-lg sm:max-w-md sm:px-[4.2rem] sm:m-0 m-12 2xl:py-12 sm:py-6 h-screen flex flex-col justify-center">
+      <div className="2xl:mb-8 md:mb-4 mb-2">
         <img src={logo} className="w-16 h-16" />
-        <h1 className="text-black font-bold text-4xl">Log In</h1>
-        <p className="text-zinc-400 mt-2    ">
+        <h1 className="text-black font-bold 2xl:text-4xl text-2xl">Log In</h1>
+        <p className="text-zinc-400 mt-2 sm:text-sm">
           Don't have an account?&nbsp;
-          <span className="text-black font-bold ">Create an account</span>
+          <span className="text-black font-bold md:text-md">
+            Create an account
+          </span>
         </p>
       </div>
       <div className="flex flex-col gap-2">
@@ -29,28 +31,28 @@ const Form = () => {
         <Button icon={FaApple}>Continue with Apple</Button>
       </div>
       <div className="flex justify-center">
-        <p className="text-center text-zinc-400 py-4 w-full flex justify-between items-center before:box-content before:w-[30%] before:h-[0.05rem] before:bg-zinc-400 before:block after:box-content after:w-[30%] after:h-[0.05rem] after:bg-zinc-400 after:block">
+        <p className="md:text-sm text-center text-zinc-400 2xl:py-4 py-2 w-full flex justify-between items-center before:box-content before:w-[30%] before:h-[0.05rem] before:bg-zinc-400 before:block after:box-content after:w-[30%] after:h-[0.05rem] after:bg-zinc-400 after:block">
           OR
         </p>
       </div>
-      <form className="flex gap-4 flex-col">
+      <form className="flex 2xl:gap-4 gap-2 flex-col">
         <input
-          className="border-2 border-zinc-400 w-full p-2 rounded-md font-bold"
+          className="border-2 border-zinc-400 w-full 2xl:p-2 p-1 rounded-md font-bold sm:text-sm"
           type="text"
           name="email"
           placeholder="Email"
         ></input>
         <input
-          className="border-2 border-zinc-400 w-full p-2 rounded-md font-bold"
+          className="border-2 border-zinc-400 w-full 2xl:p-2 p-1 rounded-md font-bold sm:text-sm"
           type="password"
           name="Password"
           placeholder="Password"
         ></input>
-        <button className="bg-black text-white p-2 rounded-md hover:text-zinc-300 duration-150">
+        <button className="bg-black text-white 2xl:p-2 p-1 rounded-md hover:text-zinc-300 duration-150 sm:text-sm">
           Log In
         </button>
       </form>
-      <div className="flex flex-col my-8 gap-4">
+      <div className="flex flex-col 2xl:my-8 my-4 2xl:gap-4 gap-2 sm:text-sm">
         <p className="underline hover:no-underline cursor-pointer">
           Use single sign-on
         </p>
@@ -59,7 +61,7 @@ const Form = () => {
         </p>
       </div>
 
-      <p className="text-zinc-400 text-[0.9rem] pr-12">
+      <p className="text-zinc-400 text-[0.9rem] pr-12 sm:text-xs">
         By Clicking "Continue Apple/Goole", above you acknowledge thaat you have
         agree to our&nbsp;
         <span className="underline hover:no-underline cursor-pointer">
@@ -70,7 +72,7 @@ const Form = () => {
           Privacy Policy
         </span>
       </p>
-      <p className="text-zinc-400 text-[0.9rem] pr-12 mt-4">
+      <p className="text-zinc-400 text-[0.9rem] pr-12 md:text-xs mt-2 mb-4">
         This site is protected by reCAPTCHA and the&nbsp;
         <span className="underline hover:no-underline cursor-pointer">
           privacy policy
@@ -81,7 +83,7 @@ const Form = () => {
         </span>
         apply
       </p>
-      <div className="mt-auto gap-4 flex text-zinc-400">
+      <div className="mt-auto gap-4 flex text-zinc-400 sm:text-xs">
         <a className="cursor-pointer">Terms</a>
         <a className="cursor-pointer">Privacy</a>
         <a className="cursor-pointer">Support</a>
